744a0d973e038ac371718de2f72e211ac4bc7dd9
Popugai28 <pavvyb@icloud.com>
2020-11-29 19:30:15 +0300

    added quiz completion example

--- quizgame/src/views/Home.vue
+++ quizgame/src/views/Home.vue
@@ -9,7 +9,27 @@
       <input type="text" name="email" v-model="email" placeholder="email" />
       {{password}}
       <input type="password" name="password" v-model="password" placeholder="Password" />
-      <button type="button" v-on:click="register()">Login</button>
+      <button type="button" v-on:click="register()">Register</button>
+      <button type="button" v-on:click="login()">Login</button>
+    </div>
+    <div>
+      <input type="text" name="quizName" v-model="quizName" placeholder="quizName" />
+      <button type="button" v-on:click="findQuiz()">Find quiz</button>
+      {{ quizQuestions }}
+    </div>
+    <div>
+      Quiz Example:
+      <div>
+        <div>
+          {{text}}
+          {{options}}
+          <select class="form-control" v-model="userAnsw" :required="true">
+            <option v-for="option in options" v-bind:key="option.id">{{ option }}</option>
+          </select>
+        </div>
+        <div>{{final}}</div>
+        <button type="button" v-on:click="answer()">Answer</button>
+      </div>
     </div>
   </div>
 </template>
@@ -26,8 +46,17 @@ export default {
   },
   data() {
     return {
+      options: [],
+      answ: '',
+      userAnsw: '',
+      text: '',
       email: '',
+      final: '',
       password: '',
+      quizName: '',
+      finalResult: '',
+      quizQuestions: {},
+      quizData: {},
       quizlist: []
     };
   },
@@ -47,6 +76,37 @@ export default {
         // ..
       });
     },
+    async answer(){
+      if (this.userAnsw===this.answ){
+        this.final = "Correct!";
+      }
+      else {
+        this.final = "Wrong! Correct answer is: " + this.answ;
+      }
+    },
+    async login() {
+      console.log(auth.currentUser.uid);
+      auth.signInWithEmailAndPassword(this.email, this.password)
+      .then((user) => {
+        console.log(user);
+        console.log(auth.currentUser.uid);
+        // Signed in 
+        // ...
+      })
+      .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(errorCode, errorMessage);
+        // ..
+      });
+    },
+    async findQuiz() {
+      const quizFinder = db.collection('quizzes').doc(this.quizName);
+      const snapshot = await quizFinder.get();
+      const safeJsonStringify = require('safe-json-stringify');
+      console.log(safeJsonStringify(snapshot.data()));
+      console.log("GOT_CERTAIN");
+  },
   },
   async beforeMount() {
     const citiesRef = db.collection('quizzes');
@@ -59,6 +119,22 @@ export default {
       this.quizlist.push(safeJsonStringify(doc.data()));
     });
     console.log("GOT_QUIZZES");
+
+    const quizDannie = db.collection('questions').doc('test228');
+    const quizAAA = await quizDannie.get();
+    console.log("Quizdanye");
+    console.log(safeJsonStringify(quizAAA.data()));
+    this.quizData = safeJsonStringify(quizAAA.data());
+
+    
+    this.answ = await quizAAA.data().correctAnswer;
+    const answopt = await quizAAA.data().answerOptions;
+    
+    for (let i = 0; i < answopt.length; i++) {
+      this.options.push(answopt[i]);
+    }
+
+    console.log("ddd", this.options)
   },
 }
 </script>
