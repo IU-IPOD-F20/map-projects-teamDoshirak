414a016389891b50812384694a5600bc79490c32
Popugai28 <pavvyb@icloud.com>
2020-12-02 13:11:33 +0300

    tests finished

--- quizgame/tests/unit/example.spec.js
+++ quizgame/tests/unit/example.spec.js
@@ -7,16 +7,19 @@ describe('Mounted Home', () => {
   it('button click with correct answer', () => {
     wrapper.setData({ answ: "15" })
     wrapper.setData({ userAnsw: "15" })
-    const button = wrapper.find("#answerbutton")
-    button.trigger('click')
-    expect(wrapper.vm.final).toBe('Correct!')
+    // const button = wrapper.find("#answerbutton")
+    // button.trigger('click')
+    wrapper.vm.answer()
+    // expect(wrapper.vm.quizlist).toBe('Correct!')
+    expect(wrapper.vm.$data.final).toBe('Correct!')
   });
 
-  it('button click with incorrect answer', () => {
+  it('button click with incorrect answer', async () => {
     wrapper.setData({ answ: "17" })
     wrapper.setData({ userAnsw: "15" })
-    const button = wrapper.find("#answerbutton")
-    button.trigger('click')
-    expect(wrapper.vm.final).toBe('Wrong! Correct answer is: 17')
+    // const button = wrapper.find("#answerbutton")
+    // const xxx = await button.trigger('click')
+    wrapper.vm.answer()
+    expect(wrapper.vm.$data.final).toBe('Wrong! Correct answer is: 17')
   })
 })
